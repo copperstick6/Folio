9a4691fadbb22ec179fb035b3d7e5e7045b57f8e
copperstick6 <copperstick6@gmail.com>
2017-03-08 16:44:16 -0600

    added the first button and added the form submission for manual input

--- Folio/app/src/main/java/com/folio/android/folio/savedCards.java
+++ Folio/app/src/main/java/com/folio/android/folio/savedCards.java
@@ -2,12 +2,25 @@ package com.folio.android.folio;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 
 public class savedCards extends AppCompatActivity {
+    public Button manual_input;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_saved_cards);
+        manual_input = (Button) findViewById(R.id.add_manual);
+        manual_input.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent i = new Intent(savedCards.this, Manual_Input.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
