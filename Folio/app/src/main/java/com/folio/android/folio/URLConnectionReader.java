a88f7f5cf4a9704cf123d158c95e2ad62bb114c3
copperstick6 <copperstick6@gmail.com>
2017-03-11 18:37:50 -0600

    HTTP requests work now

--- Folio/app/src/main/java/com/folio/android/folio/URLConnectionReader.java
+++ Folio/app/src/main/java/com/folio/android/folio/URLConnectionReader.java
@@ -4,6 +4,10 @@ package com.folio.android.folio;
  * Created by copperstick6 on 3/11/17.
  */
 
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+
 import java.net.*;
 import java.io.*;
 
@@ -12,17 +16,22 @@ import java.io.*;
  * the raw main address, as there can be multiple matches, so we also need to add in the state, etc
  */
 public class URLConnectionReader {
+    public String url;
+    public URLConnectionReader(String urltoRead){
+        this.url = urltoRead;
+    }
 
 
     /**
      * Method to make a default GET request to the given website
-     * @param urlToRead     This is the website to send the GET request to
+     * The Gson Package now works.
      * @return              Return a string of output in unformatted JSON object type
      * @throws Exception
      */
-    public static String getHTML(String urlToRead) throws Exception {
+    public String getHTML() throws Exception {
+        System.out.println(this.url);
         StringBuilder result = new StringBuilder();
-        URL url = new URL(urlToRead);
+        URL url = new URL(this.url);
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         conn.setRequestMethod("GET");
         BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
@@ -30,7 +39,12 @@ public class URLConnectionReader {
         while ((line = rd.readLine()) != null) {
             result.append(line);
         }
+
         rd.close();
+        Gson gson = new Gson();
+        JsonElement element = gson.fromJson (result.toString(), JsonElement.class);
+        JsonObject jsonObj = element.getAsJsonObject();
+        System.out.println(jsonObj.entrySet().size());
         return result.toString();
     }
 }
--- Folio/app/src/main/java/com/folio/android/folio/addChoiceActivity.java
+++ Folio/app/src/main/java/com/folio/android/folio/addChoiceActivity.java
@@ -1,12 +1,14 @@
 package com.folio.android.folio;
 
 import android.content.Intent;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class addChoiceActivity extends AppCompatActivity {
+    public Button OCV_input;
     public Button manual_input;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,12 +17,34 @@ public class addChoiceActivity extends AppCompatActivity {
         //Button to start the form
         //Needs migration over to a new Activity Screen where we have two buttons
         //OCV or manual add
-        manual_input = (Button) findViewById(R.id.add_OCV);
+        OCV_input = (Button) findViewById(R.id.add_OCV);
+        OCV_input.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+                Intent i = new Intent(addChoiceActivity.this, Manual_Input.class);
+                startActivity(i);
+            }
+        });
+        if (android.os.Build.VERSION.SDK_INT > 9)
+        {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
+        manual_input = (Button) findViewById(R.id.add_manual);
         manual_input.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                Intent i = new Intent(addChoiceActivity.this, Manual_Input.class);
-                startActivity(i);
+
+                URLConnectionReader connect = new URLConnectionReader("https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyCDw3K_2g9Iw-kPciTLhs-LLjUc65Lu-mU");
+                try{
+                    System.out.println("entered");
+                    connect.getHTML();
+                }
+                catch(Exception a){
+                    System.out.println("Caught");
+                    a.printStackTrace();
+                }
             }
         });
     }
