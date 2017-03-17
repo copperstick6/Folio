c12965fd50be6c4c393606f49d5544638e3f6a19
copperstick6 <copperstick6@gmail.com>
2017-03-16 22:28:39 -0500

    got list working, just need to format

--- Folio/app/src/main/java/com/folio/android/folio/cardFragment.java
+++ Folio/app/src/main/java/com/folio/android/folio/cardFragment.java
@@ -1,5 +1,6 @@
 package com.folio.android.folio;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
@@ -17,6 +18,9 @@ import java.util.ArrayList;
 
 public class cardFragment extends Fragment {
     private RecyclerView mRecyclerView;
+    public cardDB cards;
+    private cardAdapter adapter;
+
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -25,7 +29,32 @@ public class cardFragment extends Fragment {
         View v = inflater.inflate(R.layout.cardlist, container, false);
         mRecyclerView = (RecyclerView) v.findViewById(R.id.cardlist);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        updateUI();
         return v;
+    }
+    public void updateUI(){
+        cards = new cardDB(this.getContext());
+        Cursor cur = cards.getAllData();
+        ArrayList<ArrayList<String>> allCards = new ArrayList<ArrayList<String>>();
+        while(cur.moveToNext()){
+            ArrayList<String> obj1 = new ArrayList<String>();
+            obj1.add(cur.getString(0));
+            obj1.add(cur.getString(1));
+            obj1.add(cur.getString(2));
+            obj1.add(cur.getString(3));
+            obj1.add(cur.getString(4));
+            obj1.add(cur.getString(5));
+            obj1.add(cur.getString(6));
+            obj1.add(cur.getString(7));
+            obj1.add(cur.getString(8));
+            obj1.add(cur.getString(9));
+            obj1.add(cur.getString(10));
+            allCards.add(obj1);
+        }
+        adapter = new cardAdapter(allCards);
+        mRecyclerView.setAdapter(adapter);
+
+
     }
     private class cardHolder extends RecyclerView.ViewHolder{
         public TextView mTitleTextView;
@@ -48,7 +77,7 @@ public class cardFragment extends Fragment {
         @Override
         public void onBindViewHolder(cardHolder holder, int position){
             ArrayList<String> curObject = allCards.get(position);
-            holder.mTitleTextView.setText(curObject.get(1));
+            holder.mTitleTextView.setText(curObject.get(1) + " " + curObject.get(2));
         }
         @Override
         public int getItemCount(){
